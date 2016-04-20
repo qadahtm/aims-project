56377e71626d3be3a047d1d3f6260b8c29782c6d
Thamir Qadah <qadah.thamir@gmail.com>
2016-04-19 22:11:58 -0400

    fix for block transaction table

--- mtx.sql
+++ mtx.sql
@@ -1,3 +1,6 @@
+create table blocked_transactions_table
+(transaction_id xid, detection_time_stamp timestamp) without oids;
+
 create table malicious_transactions_table
 (transaction_id xid, detection_time_stamp timestamp) without oids;
 
@@ -7,6 +10,7 @@ DECLARE
     dummy xid := etxid;
     ts_current timestamp := current_timestamp;
 BEGIN
+  insert into blocked_transactions_table values (dummy, ts_current);
   insert into malicious_transactions_table(transaction_id, detection_time_stamp) 
         values (dummy, ts_current);
 END;
